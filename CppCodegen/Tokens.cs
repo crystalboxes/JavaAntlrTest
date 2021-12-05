28b7220055ddf8061368b2b51f026ef2d60ecb2c
Oleg Pustovit <crystalboxesgfx@gmail.com>
2021-12-05 21:44:31 +0200

    update tokens dictionary

--- CppCodegen/Tokens.cs
+++ CppCodegen/Tokens.cs
@@ -2,9 +2,6 @@ namespace JavaAST.CppCodegen
 {
     public static class Tokens
     {
-        public static string PUBLIC = "public";
-        public static string STATIC = "static";
-        public static string CLASS = "class";
         public static string SEMICOLON = ";";
         public static string COLON = ":";
         public static string DOUBLE_COLON = "::";
@@ -12,5 +9,15 @@ namespace JavaAST.CppCodegen
         public static string BRACE_CLOSE = "}";
         public static string PAREN_OPEN = "(";
         public static string PAREN_CLOSE = ")";
+        public static string SPACE = " ";
+        public static string HASH = "#";
+        public static string ENDL = "\n";
+        public static string LT = "<";
+        public static string GT = ">";
+        public static string PRAGMA = "pragma";
+        public static string INCLUDE = "include";
+        public static string PUBLIC = "public";
+        public static string STATIC = "static";
+        public static string CLASS = "class";
     }
 }
