8a3a3fa177d4c7076c85a7ed62fa2f08799b8ed8
Oleg Pustovit <crystalboxesgfx@gmail.com>
2021-12-05 21:46:55 +0200

    implement header output

--- CppCodegen/CodeGenerator.cs
+++ CppCodegen/CodeGenerator.cs
@@ -4,10 +4,48 @@ namespace JavaAST.CppCodegen
 {
     public class CodeGenerator
     {
-        UnitDefinition _unit;
+        public string Header { get { return _header.ToString(); } }
+        public string Implementation { get { return _source.ToString(); } }
         public CodeGenerator(UnitDefinition unit)
         {
             _unit = unit;
+
+            BuildHeader();
         }
+
+
+        void BuildHeader()
+        {
+            _header.AddPragmaOnce();
+            _header.AddInclude("runtime.h");
+
+            _header.AddNewLine();
+
+            foreach (var classDefinition in _unit.Classes)
+            {
+                BuildClassDeclaration(classDefinition);
+            }
+        }
+
+        void BuildClassDeclaration(ClassDefinition classDefinition)
+        {
+            _header.AddLine(Tokens.CLASS, Tokens.SPACE, classDefinition.Name!, Tokens.SPACE, Tokens.BRACE_OPEN);
+            _header.AddLine(Tokens.PUBLIC, Tokens.COLON);
+            _header.AddNewLine();
+            _header.PushIndent();
+
+            foreach (var childClassDefinition in classDefinition.Classes)
+            {
+                BuildClassDeclaration(childClassDefinition);
+            }
+
+            _header.PopIndent();
+            _header.AddLine(Tokens.BRACE_CLOSE, Tokens.SEMICOLON);
+        }
+
+        UnitDefinition _unit;
+
+        SourceBuilder _header = new();
+        SourceBuilder _source = new();
     }
 }
