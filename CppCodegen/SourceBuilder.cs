1317dd8ad1727f132138cb7d26290c74198ef690
Oleg Pustovit <crystalboxesgfx@gmail.com>
2021-12-05 21:46:40 +0200

    add source builder

---
+++ CppCodegen/SourceBuilder.cs
@@ -0,0 +1,23 @@
+namespace JavaAST.CppCodegen
+{
+
+
+    public class SourceBuilder : TokenBuildStream
+    {
+        public void AddNewLine()
+        {
+            Add(Tokens.ENDL);
+        }
+        public void AddPragmaOnce()
+        {
+            AddLine(Tokens.HASH, Tokens.PRAGMA, Tokens.SPACE, "once");
+        }
+
+        public void AddInclude(string path)
+        {
+            AddLine(Tokens.HASH, Tokens.INCLUDE, Tokens.SPACE, Tokens.LT, path, Tokens.GT);
+        }
+
+       
+    }
+}
